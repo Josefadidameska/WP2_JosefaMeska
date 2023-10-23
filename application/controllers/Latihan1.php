5e51011d0c3050b4d8f0d65d7671ba6f2c22ae8f
Josefadidameska <josefadidameskabanjarnahor@gmail.com>
2023-10-24 04:18:06 +0700

    pert 1, contoh 1

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,10 @@
+<?php
+
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Nama saya Josefa Didameska Banjarnahor
+        Saya Tinggal di Cluster vernonia residence blok DH 03";
+    }
+}
