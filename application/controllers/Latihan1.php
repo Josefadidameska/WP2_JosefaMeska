69b4e0de2834a49011f35ef329500ccf027173ae
Josefadidameska <josefadidameskabanjarnahor@gmail.com>
2023-10-24 06:44:47 +0700

    pert2, menggunakan mvc, contoh 3

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -4,8 +4,7 @@ class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "Nama saya Josefa Didameska Banjarnahor
-        Saya Tinggal di Cluster vernonia residence blok DH 03";
+        echo "Selamat datang, selamat belajar web proramming";
     }
 
     public function penjumlahan($n1, $n2)
---
+++ application/views/view-latihan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+<div>Halo Kawan, yuk belajar Web Programming.</div>
+<div>Nilai 1 = <?= $n1 ?></div>
+<div>Nilai 2 = <?= $n2 ?></div>
+<div>Ini Hasil dari pemodelan metode penjumlahan <?= $n1 ?> + <?= $n2 ?> = <?= $hasil ?></div>
+</body>
+</html>
